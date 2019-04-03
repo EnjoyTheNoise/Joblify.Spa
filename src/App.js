0824e5a036993a63e21837ae68368a15abc0e7bc
Bartosz97 <bartoszdudek5@wp.pl>
2019-04-03 13:17:47 +0200

    Added LoginForm and basic Navbar with HomePage.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Switch } from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import './App.css';
+import LoginFormContainer from './containers/LoginFormContainer';
+import HomePage from './components/HomePage';
+import Navbar from './components/Navbar';
 
 class App extends Component {
   render() {
     return (
+      <Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <Navbar/>
+        <div className="container content">
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/login" component={LoginFormContainer}/>
+        </Switch>
+        </div>
       </div>
+      </Router>
     );
   }
 }
