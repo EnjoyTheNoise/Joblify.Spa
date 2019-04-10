28bc1d49f4c73ba4e5b3b7bb7d9bbde35bc1b0dc
jacek <jacek15157@gmail.com>
2019-04-10 11:28:49 +0200

    Added picture, buttons and fixed folders structure

--- src/components/HomePage/Page.js
+++ src/components/HomePage/Page.js
@@ -1,14 +1,23 @@
 import React from "react";
 import "./Page.css";
-import SingleOpinion from "../SingleOpinion";
-const TopPageHalf = props => {
+import SingleOpinion from "../SingleOpinion/SingleOpinion";
+import "../SingleOpinion/SingleOpinion.css";
+import hammer from "./hammer.png";
+const Page = () => {
   return (
     <div className="gridPage">
       <div id="top">
+        <img src={hammer} className="hammer" />
+        <button type="button" className="btn btn-primary btn-lg btnPos">
+          Szukam pomocy
+        </button>
+        <button type="button" className="btn btn-primary btn-lg btnPos">
+          Potrzebuję pomoc
+        </button>
         <SingleOpinion />
       </div>
     </div>
   );
 };
 
-export default TopPageHalf;
+export default Page;
