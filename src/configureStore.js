f07d030dbfa1921f4bffc7d8139322bfaeb03db3
Unknown <jankos.pdp@gmail.com>
2019-04-26 20:31:22 +0200

    refactored configureStore
    registered new middlewares, now method returns store and sagaMiddleware as well

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,17 +1,34 @@
 import { createStore, compose, applyMiddleware, combineReducers } from "redux";
 import thunkMiddleware from "redux-thunk";
-import rootReducer from "./rootReducer";
+import createSagaMiddleware from "redux-saga";
+import { routerMiddleware } from "connected-react-router";
+import { createBrowserHistory } from "history";
+import createRootReducer from "./rootReducer";
+
+export const history = createBrowserHistory();
 
 export default function configureStore(initialState) {
   const composeEnhancers =
     (typeof window === "object" &&
       window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
     compose;
-  return createStore(
-    combineReducers({
-      ...rootReducer
-    }),
+
+  const sagaMiddleware = createSagaMiddleware();
+
+  const store = createStore(
+    createRootReducer(history),
     initialState,
-    composeEnhancers(applyMiddleware(thunkMiddleware))
+    composeEnhancers(
+      applyMiddleware(
+        thunkMiddleware,
+        sagaMiddleware,
+        routerMiddleware(history)
+      )
+    )
   );
+
+  return {
+    store,
+    sagaMiddleware
+  };
 }
