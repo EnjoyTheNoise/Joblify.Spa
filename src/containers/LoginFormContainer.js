b91c9c7535b5081f3264c146cae8e81e7a4b5c2c
Unknown <jankos.pdp@gmail.com>
2019-04-29 15:49:15 +0200

    Destructure props in container, use actions directly from props

--- src/containers/LoginFormContainer.js
+++ src/containers/LoginFormContainer.js
@@ -6,34 +6,19 @@ import * as loginActions from "../actions/LoginActions";
 import LoginForm from "../components/Login/LoginForm";
 
 class LoginFormContainer extends Component {
-  loginRequest = () => {
-    this.props.actions.loginRequest();
-  };
-
-  handleFacebookResponse = response => {
-    this.props.actions.handleFacebookResponse(response);
-  };
-
-  handleGoogleFailure = response => {
-    this.props.actions.handleGoogleFailure(response);
-  };
-
-  handleGoogleSuccess = response => {
-    this.props.actions.handleGoogleSuccess(response);
-  };
-
   render() {
+    const { isLegit, isFetching, actions } = this.props;
     return (
       <LoginForm
-        isLegit={this.props.isLegit}
-        isFetching={this.props.isFetching}
+        isLegit={isLegit}
+        isFetching={isFetching}
         facebookScopes={FACEBOOK_SCOPES}
         fbApi={API_FB_ID}
         googleApi={API_GOOGLE_ID}
-        loginRequest={this.loginRequest}
-        handleFacebookResponse={this.handleFacebookResponse}
-        handleGoogleSuccess={this.handleGoogleSuccess}
-        handleGoogleFailure={this.handleGoogleFailure}
+        loginRequest={actions.loginRequest}
+        handleFacebookResponse={actions.handleFacebookResponse}
+        handleGoogleSuccess={actions.handleGoogleSuccess}
+        handleGoogleFailure={actions.handleGoogleFailure}
       />
     );
   }
