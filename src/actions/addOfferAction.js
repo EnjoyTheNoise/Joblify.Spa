dbe303a1232270e8bd9baf8878d24eff6656f7fd
bartoszdudek <bartosz.dudek@angrynerds.pl>
2019-05-12 21:44:27 +0200

    Added httpErrorReducer

--- src/actions/addOfferAction.js
+++ src/actions/addOfferAction.js
@@ -1,5 +1,6 @@
 import axios from "axios";
 import { BASE_URL } from "../constants";
+import { handleHttpError } from "./httpErrorAction";
 
 export const GET_ALL_CATEGORIES = "GET_ALL_CATEGORIES";
 export const GET_ALL_CATEGORIES_SUCCESS = "GET_ALL_CATEGORIES_SUCCESS";
@@ -14,78 +15,84 @@ export const POST_NEW_OFFER_SUCCESS = "POST_NEW_OFFER_SUCCESS";
 export const POST_NEW_OFFER_FAILURE = "POST_NEW_OFFER_FAILURE";
 
 const getAllCategoriesSuccess = response => ({
-    type: GET_ALL_CATEGORIES_SUCCESS,
-    payload: response.data
-  });
-  
-  const getAllCategoriesFailure = error => ({
-    type: GET_ALL_CATEGORIES_FAILURE,
-    error
-  });
+  type: GET_ALL_CATEGORIES_SUCCESS,
+  payload: response.data
+});
 
-  const getAllTradesSuccess = response => ({
-    type: GET_ALL_TRADES_SUCCESS,
-    payload: response.data
-  });
-  
-  const getAllTradesFailure = error => ({
-    type: GET_ALL_TRADES_FAILURE,
-    error
-  });
+const getAllCategoriesFailure = error => ({
+  type: GET_ALL_CATEGORIES_FAILURE,
+  error
+});
 
-  const postNewOfferSuccess = response => ({
-    type: POST_NEW_OFFER_SUCCESS,
-    payload: response.data
-  });
-  
-  const postNewOfferFailure = error => ({
-    type: POST_NEW_OFFER_FAILURE,
-    error
-  });
+const getAllTradesSuccess = response => ({
+  type: GET_ALL_TRADES_SUCCESS,
+  payload: response.data
+});
+
+const getAllTradesFailure = error => ({
+  type: GET_ALL_TRADES_FAILURE,
+  error
+});
+
+const postNewOfferSuccess = response => ({
+  type: POST_NEW_OFFER_SUCCESS,
+  payload: response.data
+});
+
+const postNewOfferFailure = error => ({
+  type: POST_NEW_OFFER_FAILURE,
+  error
+});
 export const getAllCategories = props => dispatch => {
-    dispatch({type: GET_ALL_CATEGORIES});
+  dispatch({ type: GET_ALL_CATEGORIES });
 
-    return axios.get(BASE_URL+"/offer/getallcategories")
-      .then(response => { console.log(response);
-        dispatch(getAllCategoriesSuccess(response));},
-        error => {
-            if (error.response.status === 400) {
-                dispatch(getAllCategoriesFailure(error));
-            } else {
-                //dispatch(handleHttpError(error, props));
-            }
-        }
-      );
-}
+  return axios.get(BASE_URL + "/offer/getallcategories").then(
+    response => {
+      console.log(response);
+      dispatch(getAllCategoriesSuccess(response));
+    },
+    error => {
+      if (error.response.status === 400) {
+        dispatch(getAllCategoriesFailure(error));
+      } else {
+        dispatch(handleHttpError(error, props));
+      }
+    }
+  );
+};
 
 export const getAllTrades = props => dispatch => {
-  dispatch({type: GET_ALL_TRADES});
+  dispatch({ type: GET_ALL_TRADES });
 
-  return axios.get(BASE_URL+"/offer/getalltrades")
-    .then(response => { console.log(response);
-      dispatch(getAllTradesSuccess(response));},
-      error => {
-          if (error.response.status === 400) {
-              dispatch(getAllTradesFailure(error));
-          } else {
-              //dispatch(handleHttpError(error, props));
-          }
+  return axios.get(BASE_URL + "/offer/getalltrades").then(
+    response => {
+      console.log(response);
+      dispatch(getAllTradesSuccess(response));
+    },
+    error => {
+      if (error.response.status === 400) {
+        dispatch(getAllTradesFailure(error));
+      } else {
+        dispatch(handleHttpError(error, props));
       }
-    );
-}
-    
+    }
+  );
+};
+
 export const postNewOffer = (offer, props) => dispatch => {
-  dispatch({type: POST_NEW_OFFER});
-console.log(offer);
-  return axios.post(BASE_URL + "/offer/", offer) 
-    .then(response => { console.log(response);
-      dispatch(postNewOfferSuccess(response));},
-      error => {
-          if (error.response.status === 400) {
-              dispatch(postNewOfferFailure(error));
-          } else {
-              //dispatch(handleHttpError(error, props));
-          }
+  dispatch({ type: POST_NEW_OFFER });
+  console.log(offer);
+  return axios.post(BASE_URL + "/offer/", offer).then(
+    response => {
+      console.log(response);
+      dispatch(postNewOfferSuccess(response));
+    },
+    error => {
+      if (error.response.status === 400) {
+        dispatch(postNewOfferFailure(error));
+      } else {
+        dispatch(handleHttpError(error, props));
       }
-    );
-}
+    }
+  );
+};
--- src/containers/AddOfferContainer.js
+++ src/containers/AddOfferContainer.js
@@ -1,10 +1,12 @@
 import React, { Component } from "react";
-import { getAllCategories, getAllTrades, postNewOffer} from "../actions/addOfferAction";
+import {
+  getAllCategories,
+  getAllTrades,
+  postNewOffer
+} from "../actions/addOfferAction";
 import { bindActionCreators } from "redux";
 import { connect } from "react-redux";
 import AddOffer from "../components/Offer/AddOffer";
-import axios from "axios";
-import { BASE_URL } from "../constants";
 
 const initialState = {
   title: "",
@@ -16,76 +18,87 @@ const initialState = {
 };
 
 export class AddOfferContainer extends Component {
-   constructor(props) {
-     super(props);
-     this.state = initialState;
-   }
-   componentDidMount = () => {
-    this.props.actions.getAllCategories();
-  
-    this.props.actions.getAllTrades();
+  constructor(props) {
+    super(props);
+    this.state = initialState;
   }
-      onSubmit = e => {
-        e.preventDefault();
-        const offer = {
-          //userId value for tests
-          userId: 5,
-          title: this.state.title,
-          description: this.state.description,
-          price: this.state.price,
-          categoryId: this.state.categoryId,
-          tradeId: this.state.tradeId,
-          availableTime: this.state.availableTime        
-        }
+  componentDidMount = () => {
+    this.props.actions.getAllCategories();
 
-        this.props.actions.postNewOffer(
-         offer 
-        )       
-      }
-
-      handleUserInput = e => {
-        let state = this.state;
-        state[e.target.name] = e.target.value;
-        this.setState(state);    
+    this.props.actions.getAllTrades();
+  };
+  onSubmit = e => {
+    e.preventDefault();
+    const offer = {
+      //userId value for tests
+      userId: 5,
+      title: this.state.title,
+      description: this.state.description,
+      price: this.state.price,
+      categoryId: this.state.categoryId,
+      tradeId: this.state.tradeId,
+      availableTime: this.state.availableTime
     };
 
-    handleUserSelect = e => {
-      let state = this.state;
-      state[e.target.name] = e.target.selectedIndex;
-      this.setState(state);
-    }
+    this.props.actions.postNewOffer(offer);
+  };
 
-      render() {
-        const {isFetching, categories, trades, actions} = this.props;
-        const { price, title, description, tradeId, categoryId, availableTime} = this.state;
-        return (
-          <AddOffer
-          isFetching = {isFetching}
-          categories = {categories}
-          trades = {trades}
-          availableTime = {availableTime}
-          price = {price}
-          title = {title}
-          description = {description}
-          tradeId = {tradeId}
-          categoryId = {categoryId}
-          actions = {actions}
-          onSubmit = {this.onSubmit}
-          handleUserInput = {this.handleUserInput}
-          handleUserSelect = {this.handleUserSelect}
-          />
-        );
-      }
+  handleUserInput = e => {
+    let state = this.state;
+    state[e.target.name] = e.target.value;
+    this.setState(state);
+  };
+
+  handleUserSelect = e => {
+    let state = this.state;
+    state[e.target.name] = e.target.selectedIndex;
+    this.setState(state);
+  };
+
+  render() {
+    const { isFetching, categories, trades, actions } = this.props;
+    const {
+      price,
+      title,
+      description,
+      tradeId,
+      categoryId,
+      availableTime
+    } = this.state;
+    return (
+      <AddOffer
+        isFetching={isFetching}
+        categories={categories}
+        trades={trades}
+        availableTime={availableTime}
+        price={price}
+        title={title}
+        description={description}
+        tradeId={tradeId}
+        categoryId={categoryId}
+        actions={actions}
+        onSubmit={this.onSubmit}
+        handleUserInput={this.handleUserInput}
+        handleUserSelect={this.handleUserSelect}
+      />
+    );
+  }
 }
 
 const mapStateToProps = state => ({
-    isFetching: state.addOffer.isFetching,
-    categories: state.addOffer.categories,
-    trades: state.addOffer.trades,
-  });
-  
-  const mapDispatchToProps = dispatch => ({
-    actions: bindActionCreators({getAllCategories, getAllTrades, postNewOffer}, dispatch)
-  });
-  
-  export default connect(mapStateToProps, mapDispatchToProps) (AddOfferContainer);
+  isFetching: state.addOffer.isFetching,
+  categories: state.addOffer.categories,
+  trades: state.addOffer.trades
+});
+
+const mapDispatchToProps = dispatch => ({
+  actions: bindActionCreators(
+    { getAllCategories, getAllTrades, postNewOffer },
+    dispatch
+  )
+});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(AddOfferContainer);
