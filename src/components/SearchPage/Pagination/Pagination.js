6dcc1c77dd2e9c86d91b4427186cda5fc68a5653
Szymon Pituła <deesugfx@gmail.com>
2019-05-20 15:28:31 +0200

    Improved pagination

--- src/components/SearchPage/Pagination/Pagination.js
+++ src/components/SearchPage/Pagination/Pagination.js
@@ -1,25 +1,46 @@
-import React from "react";
+import React, { Component } from "react";
 import "./Pagination.css";
 
-const Pagination = props => {
-  const currentPage = props.currentPage;
+class Pagination extends Component {
+  render() {
+    const currentPage = parseInt(this.props.currentPage);
+    const totalItems = parseInt(this.props.totalItems);
+    const pageSize = 10;
+    const totalPages = Math.ceil(totalItems / pageSize);
+    let table = [];
+    let startPage, endPage;
 
-  const a = (
-    <div className="pagination">
-      <a href="#">&laquo;</a>
-      <a href="#">1</a>
-      <a className="active" href="#">
-        {currentPage}
-      </a>
-      <a href="#">3</a>
-      <a href="#">4</a>
-      <a href="#">5</a>
-      <a href="#">6</a>
-      <a href="#">&raquo;</a>
-    </div>
-  );
+    if (totalPages <= 10) {
+      // less than 10 total pages so show all
+      startPage = 1;
+      endPage = totalPages;
+    } else {
+      // more than 10 total pages so calculate start and end pages
+      if (currentPage <= 6) {
+        startPage = 1;
+        endPage = 10;
+      } else if (currentPage + 4 >= totalPages) {
+        startPage = totalPages - 9;
+        endPage = totalPages;
+      } else {
+        startPage = currentPage - 5;
+        endPage = currentPage + 4;
+      }
+    }
 
-  return a;
-};
+    for (let i = startPage; i <= endPage; i++) {
+      let k = i === currentPage ? <a href="#" className="active">{i}</a> : <a href="#">{i}</a>;
+      table.push(k);
+    }
+
+    return (
+      <div className="pagination">
+        <a href="#">&laquo;</a>
+        {table}
+        <a href="#">&raquo;</a>
+      </div>
+    );
+  }
+}
 
 export default Pagination;
--- src/components/SearchPage/SearchPage.js
+++ src/components/SearchPage/SearchPage.js
@@ -9,6 +9,177 @@ class SearchPage extends Component {
       this.props.option === "employers" ? "Pracodawcy" : "Pracobiorcy";
     const filterState = this.props.filter;
 
+    const mock = [
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />,
+      <Offer
+        firstName="Andrzej"
+        lastName="Poniedzielski"
+        description="Poszukuje poszukiwacza poszukiwań"
+        phone="663883881"
+        price="107"
+      />
+    ];
+
     return (
       <div className="row">
         <p className="phraseSearchPage">
@@ -30,49 +201,8 @@ class SearchPage extends Component {
         </form>
 
         <div className="offers">
-          <Offer
-            firstName="Andrzej"
-            lastName="Poniedzielski"
-            description="Poszukuje poszukiwacza poszukiwań"
-            phone="663883881"
-            price="107"
-          />
-          <Offer
-            firstName="Andrzej"
-            lastName="Poniedzielski"
-            description="Poszukuje poszukiwacza poszukiwań"
-            phone="663883881"
-            price="107"
-          />
-          <Offer
-            firstName="Andrzej"
-            lastName="Poniedzielski"
-            description="Poszukuje poszukiwacza poszukiwań"
-            phone="663883881"
-            price="107"
-          />
-          <Offer
-            firstName="Andrzej"
-            lastName="Poniedzielski"
-            description="Poszukuje poszukiwacza poszukiwań"
-            phone="663883881"
-            price="107"
-          />
-          <Offer
-            firstName="Andrzej"
-            lastName="Poniedzielski"
-            description="Poszukuje poszukiwacza poszukiwań"
-            phone="663883881"
-            price="107"
-          />
-          <Offer
-            firstName="Mariusz"
-            lastName="Piguła"
-            description="Kurde czego ja poszukuje szczęścia ok xD"
-            phone="6686838182"
-            price="0"
-          />
-          <Pagination currentPage={this.props.page} />
+          {mock}
+          <Pagination currentPage={this.props.page} totalItems={mock.length} />
         </div>
       </div>
     );
