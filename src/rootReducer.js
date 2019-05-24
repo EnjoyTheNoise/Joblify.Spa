e8922400969a1a0c4b4570f9b1d9ea17747f7560
Unknown <jankos.pdp@gmail.com>
2019-05-25 00:50:37 +0200

    registered new reducers in root

--- src/rootReducer.js
+++ src/rootReducer.js
@@ -2,10 +2,16 @@ import { combineReducers } from "redux";
 import { connectRouter } from "connected-react-router";
 import facebookLogin from "./reducers/FacebookLoginReducer";
 import googleLogin from "./reducers/GoogleLoginReducer";
+import login from "./reducers/LoginReducer";
+import edit from "./reducers/EditProfileReducer";
+import user from "./reducers/UserReducer";
 
 const createRootReducer = history =>
   combineReducers({
     router: connectRouter(history),
+    user,
+    login,
+    edit,
     facebookLogin,
     googleLogin
   });
